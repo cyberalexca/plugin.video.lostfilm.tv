0f46ee838471e54870aabca1c7ab475bbeac7045
CyberAlexCa <cyberalex@gmail.com>
2016-02-24 16:04:59 -0500

    no message

--- resources/lib/lostfilm/scraper.py
+++ resources/lib/lostfilm/scraper.py
@@ -314,6 +314,7 @@ class LostFilmScraper(AbstractScraper):
 
 
 def parse_title(t):
+    print(t)
     title, original_title = re.findall('^(.*?)\s*(?:\((.*)\)\.?)?$', t)[0]
     return title, original_title
 
