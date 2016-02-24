22094e74d1185ee5b8574ebbb62a18edd8a967f8
CyberAlexCa <cyberalex@gmail.com>
2016-02-24 17:00:44 -0500

    no message

--- resources/lib/lostfilm/scraper.py
+++ resources/lib/lostfilm/scraper.py
@@ -70,7 +70,7 @@ TorrentLink = namedtuple('TorrentLink', ['quality', 'url', 'size'])
 
 
 class LostFilmScraper(AbstractScraper):
-    BASE_URL = "http://babydomik.no-ip.info"
+    BASE_URL = "http://www.lostfilm.tv"
     LOGIN_URL = "http://login1.bogi.ru/login.php"
     BLOCKED_MESSAGE = "Контент недоступен на территории Российской Федерации"
 
